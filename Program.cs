76e043a992c1893ad3ac4896eacbf7fc24244989
Daniel Bellissimo-Devenney <bellissimo38@gmail.com>
2025-02-18 21:13:52 -0500

    User stories 1 and 2

--- Program.cs
+++ Program.cs
@@ -1,9 +1,44 @@
-﻿namespace FlightsDemo;
+﻿using Flights;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+namespace FlightsDemo;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // User Story 1
+        HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+
+        builder.Services.TryAddSingleton<IFlightsService, FlightsService>();
+        builder.Services.TryAddSingleton<IFlightsDAL, FlightsDAL>();
+
+        var host = builder.Build();
+
+        Task run = host.RunAsync();
+
+        await Task.WhenAny(run, Task.Run(async () => await ControlLoop(host)));
+
+        await host.StopAsync();
+    }
+
+    public async static Task ControlLoop(IHost host)
+    {
+        while(true)
+        {
+            string input = Console.ReadLine();
+            switch(input){
+                case "":
+                    await host.StopAsync();
+                    break;
+                case "1":
+                    host.Services.GetRequiredService<IFlightsService>().PrintFlightSchedule();
+                    break;
+                case "2":
+                    host.Services.GetRequiredService<IFlightsService>().PrintOrderItineraries();
+                    break;
+            }
+        }    
     }
 }
